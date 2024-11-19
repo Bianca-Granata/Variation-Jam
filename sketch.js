6ef17f6a9edefcf58a733972bc1857b13c9c3f22
Bianca-Granata <biancag02@outlook.com>
2024-11-19 13:15:00 -0800

    description of the game and listing of potential features to include

---
+++ sketch.js
@@ -0,0 +1,17 @@
+/** Chess practice board
+ * 
+ * Bianca Granata
+ * 
+ * Chess practice board that enables the user to interact with both sides 
+ * of the board to practice their chess tecniques
+ * 
+ * 
+ */
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
