e969cbfd9ae60e4ce9aa966985e9997b38226717
Bianca-Granata <biancag02@outlook.com>
2024-12-03 13:50:28 -0800

    changing the game altogether- starting over

--- sketch.js
+++ sketch.js
@@ -1,17 +1,41 @@
-/** Chess practice board
+/** snake game variation
  * 
  * Bianca Granata
  * 
- * Chess practice board that enables the user to interact with both sides 
- * of the board to practice their chess tecniques
+ * the snake game variation is similar to the typical game but with a twist
  * 
  * 
+ * variation 1
+ * The "food" in which the snake has to eat moves, therefore the snake has to catch it
+ * 
+ * variation 2 
+ * snake game but reverse, the snake has to avoid being "caught" by the food
+ * 
+ * variation 3
+ * 
+ * 
+ * Made with p5
+ * https://p5js.org/
  */
 
+
+//creates the canvas
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(640, 480);
+}
+//adding all the pieces together
+function draw() {
+  background("#000000");
+  drawBoard();
+}
+//creating using functions 
+function drawBoard(x, y, size) {
+  push();
+  stroke();
+  square();
+  color();
+
+
+
 }
 
-function draw() {
-  background(220);
-}
